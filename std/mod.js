db2130e569e28bfc34cc6466ed04ba3d560ca028
soyhi <2pyowo@gmail.com>
2022-09-07 21:48:39 +0900

    Update mod.js

--- std/mod.js
+++ std/mod.js
@@ -1,14 +1,21 @@
 const A5738 = /^\*$/
 const D364 = /^def(ault)?$/
 const S76 = /(http(s))?:(\/{2})?(\S+\.\w+)\/((.+)\.(js))/
-const I8736 = /^\.\S+(\.js(on)?)$/
+const I8736 = /^\.\S+(\.js)$/
 
 function main() {
     function diag(data) {
         if (S76.test(data) || I8736.test(data)) {
-            AddLibrary(data)
+            AddLibraries(data)
         } else if (D364.test(data)) {
-            AddLibrary('./mod/std/collection.json')
+            AddLibraries(
+                './std/bind.js',
+                './std/clear.js',
+                './std/close.js',
+                './std/help.js',
+                './std/mod.js',
+                './std/unmod.js',
+                './e/mt.js')
         } else if (A5738.test(data)) {
             const entry = Object.entries(glib)
             entry.forEach(([name, func]) => lib[name] = func)
