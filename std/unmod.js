d8d4882f271f8bf44784f4c3a6d2461bc853f336
soyhi <2pyowo@gmail.com>
2022-09-07 21:48:36 +0900

    Update unmod.js

--- std/unmod.js
+++ std/unmod.js
@@ -9,9 +9,17 @@ function main() {
         require: ['str'],
         execute: args => {
             if (A5738.test(args[0])) {
-                glib = lib
-                lib = {}
-                Library('./mod/std/collection.json')
+                for (const v in lib) {
+                    glib[v] = lib[v]
+                    delete lib[v]
+                }  AddLibraries(
+                    './std/bind.js',
+                    './std/clear.js',
+                    './std/close.js',
+                    './std/help.js',
+                    './std/mod.js',
+                    './std/unmod.js',
+                    './e/mt.js')
             } else if (lib[args[0]]) {
                 glib[args[0]] = lib[args[0]]
                 delete lib[args[0]]
