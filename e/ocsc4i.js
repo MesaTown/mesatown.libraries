f6090a26774fa6fba7970a22265feb8be67a6da5
soyhi <2pyowo@gmail.com>
2022-10-18 07:43:13 +0900

    Update ocsc4i.js

--- e/ocsc4i.js
+++ e/ocsc4i.js
@@ -4,15 +4,7 @@ function main() {
         marker: null,
         hidden: true,
         require: ['any*'],
-        execute: args => {
-            if (SHA256(args[0]) === 'e50e6251329bcf16e1b84a63b96e2299574eb44748a3ca3b7c49f3cf9cfb6a82') {
-                cli('ocsc4i')
-                openRender()
-            }
-        },
-        onload: () => {
-            AddLibraries('./e/lab.js')
-        },
-        oncomplete: () => closeRender()
+        execute: () => {},
+        onload: () => AddLibraries('./e/lab.js')
     }
 }
